9e773af2334ec1cffa3fea3d6fc8c00b36ea596e
Johan Mosquerae <kira1974.dev@gmail.com>
2025-03-19 10:58:34 -0500

    Actualizacion pequeña en la url para registrar dispositivos

--- backend/iot/urls.py
+++ backend/iot/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RegisterIoTDevice
 
 urlpatterns = [
-    path('register/', RegisterIoTDevice.as_view(), name='registrar-dispositivo-iot'),
+    path('register', RegisterIoTDevice.as_view(), name='registrar-dispositivo-iot'),
 ]
