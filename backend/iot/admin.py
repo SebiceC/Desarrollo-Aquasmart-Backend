ec6bbce384f78cef54c0cff8f1ff56db606b28bc
Johan Mosquerae <kira1974.dev@gmail.com>
2025-03-19 14:36:38 -0500

    Se actualizo la vista del administrador  con el id del dispositivo.

--- backend/iot/admin.py
+++ backend/iot/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from .models import IoTDevice  
-
+from .models import IoTDevice
 
 @admin.register(IoTDevice)
 class IoTDeviceAdmin(admin.ModelAdmin):
-    list_display = ('id_plot', 'name', 'device_type', 'is_active', 'characteristics', 'id_lot')
+    list_display = ('device_id', 'id_plot', 'name', 'device_type', 'is_active', 'characteristics', 'id_lot')
     list_filter = ('device_type', 'is_active') 
-    search_fields = ('name', 'id_plot', 'id_lot') 
-
- 
+    search_fields = ('name', 'id_plot', 'id_lot', 'device_id')  
