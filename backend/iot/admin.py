a7cc48b35f307dfa9f5ed3c2432e8aac5e70a89b
The_Viga <rojas_exp@hotmail.com>
2025-03-19 17:25:29 -0500

    Se añadió la vista de admin de django para el crud de los dispositivos iot

--- backend/iot/admin.py
+++ backend/iot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Device
 
-# Register your models here.
+# Registrar el modelo Device
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'characteristics', 'id_predio', 'id_lote')  # Campos a mostrar en la lista
+    search_fields = ('name', 'characteristics')  # Campos por los que se puede buscar
+    list_filter = ('id_predio', 'id_lote')  # Filtros laterales
