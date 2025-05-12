dc19ecf6e452615363fce265d343566618b6a4da
Johan Mosquerae <kira1974.dev@gmail.com>
2025-05-12 13:02:16 -0500

    Merge branch 'refactor/modulo4-modelos-y-serializadores' of https://github.com/SebiceC/Desarrollo-Aquasmart-Backend into refactor/modulo4-modelos-y-serializadores

--- backend/communication/assigment_maintenance/views.py
+++ backend/communication/assigment_maintenance/views.py
@@ -5,8 +5,6 @@ from rest_framework.generics import (
     RetrieveAPIView, CreateAPIView, ListAPIView
 )
 from rest_framework.permissions import IsAuthenticated, BasePermission, IsAdminUser
-=======
-from rest_framework.permissions import IsAuthenticated, BasePermission
 from django.contrib.auth import get_user_model
 from communication.requests.models import FlowRequest
 from communication.requests.serializers import FlowRequestSerializer
