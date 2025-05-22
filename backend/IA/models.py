29c144aed4551053607d0bdcfbfcec66b1d59710
Sanviv <santiagoviveros18@gmail.com>
2025-05-22 15:09:06 -0500

    refactor(models): Mejorar las opciones del modelo y las definiciones de campos para mayor claridad y coherencia

--- backend/IA/models.py
+++ backend/IA/models.py
@@ -12,7 +12,7 @@ class ClimateRecord(models.Model):
     tempmin = models.FloatField()
     precip = models.FloatField()
     precipprob = models.FloatField()
-    precipcover = models.FloatField() 
+    precipcover = models.FloatField()
     windgust  = models.FloatField()
     windspeed = models.FloatField()
     pressure = models.FloatField()
@@ -23,6 +23,10 @@ class ClimateRecord(models.Model):
     luminiscencia = models.FloatField()
     final_date = models.DateTimeField()
 
+    class Meta:
+        verbose_name = "Registro climático"
+        verbose_name_plural = "Registros climáticos"
+
     def __str__(self):
         return f"Registro de clima desde {self.datetime} hasta {self.final_date}"
 
@@ -32,7 +36,7 @@ class ClimateRecord(models.Model):
             base_date= self.datetime.date()
             sunrise = datetime.combine(base_date, self.sunrise)
             sunset = datetime.combine(base_date, self.sunset)
-            diference= sunset - sunrise
+            diference = sunset - sunrise
             luminiscencia_hours = round(diference.total_seconds() / 3600, 2)
             self.luminiscencia = luminiscencia_hours
 
@@ -45,18 +49,22 @@ class ClimateRecord(models.Model):
 class ConsuptionPredictionLot(models.Model):
     ''' Modelo que almacena la predicción de consumo de un lote '''
     class PeriodTime(models.TextChoices):
-        ONE = "1",("1")
-        THREE = "3",("3")
-        SIX = "6",("6")
+        ONE = "1", ("1")
+        THREE = "3", ("3")
+        SIX = "6", ("6")
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, verbose_name="Documento usuario que hizo la peticion", help_text="Documento usuario que hizo la peticion" )
-    lot = models.ForeignKey(Lot, on_delete=models.CASCADE, verbose_name="Id del lote", help_text="id del lote")    
-    period_time = models.CharField(max_length=1,choices=PeriodTime,verbose_name="Tiempo a elegir", help_text="Tiempo a elegir")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion de la prediccion", help_text="Fecha de creacion de la prediccion")
-    date_prediction = models.DateField(null=True, verbose_name="Meses de prediccion",help_text="Meses de prediccion")
-    consumption_prediction = models.FloatField( verbose_name="Consumo predecido",help_text="Consumo predecido")
-    code_prediction = models.CharField(max_length=20, verbose_name="Codigo para las predicciones", help_text="Codigo de las predicciones")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Usuario solicitante", help_text="Usuario que solicita la predicción" )
+    lot = models.ForeignKey(Lot, on_delete=models.CASCADE, verbose_name="Lote", help_text="Lote al que se le realiza la predicción")
+    period_time = models.CharField(max_length=1, choices=PeriodTime, verbose_name="Periodo mensual de predicción", help_text="Periodo de tiempo (meses) a predecir")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación de la prediccion", help_text="Fecha de creacion de la predicción")
+    date_prediction = models.DateField(null=True, verbose_name="Meses de predicción", help_text="Meses de predicción")
+    consumption_prediction = models.FloatField(verbose_name="Consumo predecido", help_text="Consumo predecido")
+    code_prediction = models.CharField(max_length=20, verbose_name="Código para las predicciones", help_text="Código de las predicciones")
     final_date = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Predicción de consumo de lote"
+        verbose_name_plural = "Predicción de consumo de lotes"
 
     def __str__(self):
         return f"{self.code_prediction} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
